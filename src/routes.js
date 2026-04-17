194a05b5457a25bce6ad8335b216cbd358b2964b
Jhordan Bandeira <jhordanbandeira1@gmail.com>
2026-04-17 10:24:09 -0300

    feat: adiciona rota POST /tasks para criação de tarefas

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+
+const database = new Database()
+
+export const routes = [
+  // CREATE
+  {
+    method: 'POST',
+    path: /^\/tasks$/,
+    handler: async (req, res) => {
+      let body = ''
+
+      for await (const chunk of req) {
+        body += chunk
+      }
+
+      const { title, description } = JSON.parse(body)
+
+      if (!title || !description) {
+        return res.writeHead(400).end('Title e Description são obrigatórios')
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+
+      database.insert(task)
+
+      return res.writeHead(201).end()
+    }
+  },
+]
