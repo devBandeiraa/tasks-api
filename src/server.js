219cf3b000bd8e96ba2708f72a2a7da080ce45b3
Jhordan Bandeira <jhordanbandeira1@gmail.com>
2026-04-16 23:03:32 -0300

    feat: cria servidor HTTP base com roteamento manual

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import http from 'node:http'
+import { routes } from './routes.js'
+
+const server = http.createServer(async (req, res) => {
+    const route = routes.find(route => {
+        return route.method === req.method && route.path.test(req.url)
+    })
+
+    if (route){
+        const routeParams = req.url.match(route.path)
+
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+        req.query = query
+
+        await route.handler(req, res)
+        return
+    }
+
+    return res.writeHead(404).end('Not Found')
+})
+
+server.listen(3333)
