1be3f4b16fc345c7740cc2cd906d421c4f6c4e75
Jhordan Bandeira <jhordanbandeira1@gmail.com>
2026-04-17 10:18:52 -0300

    feat: implementa classe Database para armazenamento de tarefas

---
+++ src/database.js
@@ -0,0 +1,33 @@
+export class Database {
+    #task = []
+
+    select(search){
+        if (!search) return this.#task
+
+        return this.#task.filter(task => {
+            return (
+                task.title.includes(search) ||
+                task.description.includes(search)
+            )
+        })
+    }
+
+    insert(task){
+        this.#task.push(task)
+    }
+
+    findById(id){
+        return this.#task.find(task => task.id === id)
+    }
+
+    update(id, data){
+        const task = this.findById(id)
+        Object.assign(task, data)
+    }
+
+    delete(id){
+        this.#task = this.#tasks.filter(task => task.id !== id)
+    }
+
+
+}
